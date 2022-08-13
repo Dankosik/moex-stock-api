c71dfa0ba21f70b03ff7ff216b55d143e909cd68
Koryto Daniil <DKoryto@alfabank.ru>
2022-08-14 02:18:33 +1000

    added support for cache coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven { url = uri("https://dl.bintray.com/konrad-kaminski/maven") }
 }
 
 extra["springCloudVersion"] = "2021.0.3"
@@ -29,6 +31,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("io.github.microutils:kotlin-logging:2.1.21")
+    implementation("org.springframework.kotlin:spring-kotlin-coroutine:0.3.7")
 
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
